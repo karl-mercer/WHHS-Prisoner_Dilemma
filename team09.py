b614bb8e76079bf78c88d7a8385b7d1bb30299ca
hitthatyeet28billiontimesinarow <fbaker7124@greenvilleschools.us>
2018-03-16 13:42:15 -0400

    Added team name for team09

--- team09.py
+++ team09.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = ('teamSkylar')
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'A good strat'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
