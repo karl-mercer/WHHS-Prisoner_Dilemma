02c71d551368256f03f31fa2f837c860f6d5547d
ya boi <ebolli6967@greenvilleschools.us>
2018-03-16 13:30:31 -0400

    added a team name to team2

--- team02.py
+++ team02.py
@@ -1,12 +1,12 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
+#     team_name: Big Dogs
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'big_dogs' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
