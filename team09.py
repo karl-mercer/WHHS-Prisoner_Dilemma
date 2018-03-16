ba164faef4cc5c136172d80e9dcf499ad37e4738
Baker <fbaker7124@greenville.k12.sc.us>
2018-03-16 13:37:02 -0400

    teamSkylar has been born

--- team09.py
+++ team09.py
@@ -1,4 +1,4 @@
-####
++####
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = ('teamSkylar')
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
