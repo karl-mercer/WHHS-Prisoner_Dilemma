6824ac8f1599557dcafabfbf727e6d9fe196c354
Aspdin Jr <caspdi4154@greenville.k12.sc.us>
2018-03-20 13:00:18 -0400

    Added Team Name to Team04.py

--- team04.py
+++ team04.py
@@ -1,6 +1,6 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
+#     team_name: SledGang
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
