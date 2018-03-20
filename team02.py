00efc6f7f559a9b185a59492e3e8e4483be6d5d3
ya boi <ebolli6967@greenvilleschools.us>
2018-03-20 13:19:11 -0400

    edited the code

--- team02.py
+++ team02.py
@@ -14,7 +14,7 @@ def move(my_history, their_history, my_score, their_score):
    #if their_history[-1] == 'c':
     # print 'c'
     
-    if my_score == '0':
+    if my_score == 0:
         return 'c'
     else:
         return their_history [-1]
