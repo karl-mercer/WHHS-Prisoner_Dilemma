71d3281f8246cc0368d4cf071fc3207dff10aaad
DannyDeCheeto <gbolin5713@greenvilleschools.us>
2018-03-20 13:25:00 -0400

    Fixed some stuff

--- team05.py
+++ team05.py
@@ -33,9 +33,10 @@ def move(my_history, their_history, my_score, their_score):
         return 'c'
     elif their_history[-1]=='c':
         return 'b'
-    else:
+    elif 'bbb' in their_history:
+        return 'b'
+    elif 'cccccccc' in their_history:
         return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
