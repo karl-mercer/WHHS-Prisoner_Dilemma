25202f978ec43321124c7ade15ca867bb9ca4373
Craig Aspdin <caspdi4154@greenvilleschools.us>
2018-03-20 13:52:50 -0400

    Added Some More Code

--- team04.py
+++ team04.py
@@ -1,13 +1,13 @@
 ####
 # Each team's file must define four tokens:
 #     team_name: SledGang
-#     strategy_name: a string
+#     strategy_name: 
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+import random
+team_name = 'SledGang' # Only 10 chars displayed.
+strategy_name = ''
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -25,8 +25,20 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if my_score > their_score:
+        return random.choice('b''c')
+    if their_score > my_score:
+        return ('b')
+    if their_score == my_score:
+        return ('b')
+    if my_history == '':
+        return('c')
+
+
+def move2(my_history, their_history, my_score, their_score):
+          return
+
+return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
