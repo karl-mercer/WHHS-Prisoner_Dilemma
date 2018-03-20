8827feed702ee889b6123031caffca54658ddf98
Wolf <swolf8789@greenville.k12.sc.us>
2018-03-20 12:56:01 -0400

    Finished initial code

--- TeamWolf/TeamWolf.py
+++ TeamWolf/TeamWolf.py
@@ -25,6 +25,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    if their_score > my_score :
+        return 'b'
     if their_history == 'c' :
         return 'c'
     if their_history == 'b' :
