df8f4b483c0ec24795c070ab0b1bd18ffdb3ff2f
Jenny <jleho4713@greenvilleschools.us>
2018-03-20 13:44:27 -0400

    Added code

--- team12.py
+++ team12.py
@@ -22,7 +22,11 @@ def move(my_history, their_history, my_score, their_score):
             return 'b'
         else:
             return 'c'     
-
+        while their_history == 'bb':
+            return 'b'
+        else:
+            return 'c'
+    
    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
 # their_history: a string of the same length as history, possibly empty.
 # The first round between these two players is my_history[0] and their_history[0].
