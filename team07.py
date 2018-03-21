25c858bbb251eb0430f4fcf40469b57f0e262656
Code_Monkey <jhysin5177@greenvilleschools.us>
2018-03-21 13:35:18 -0400

    tean07.py

--- team07.py
+++ team07.py
@@ -3,21 +3,32 @@
 #     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+#     move: A function that returns 'c' or '
+####b'
 
 team_name = 'Code Monkey' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Codename: staight-faced'
+strategy_description = 'this strategy decides either to keep c or b based on their_history'
     
 def move(my_history, their_history, my_score, their_score):
+    if len(their_history)==0:
+        return 'c'    
+    elif(their_history[-2] is 'b') and (my_history[-1]  is 'c'):
+        return 'c'
+    elif () and ():
+        return  
+    else:
+        return 'b'
+ 
+   
+       
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,8 +36,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
