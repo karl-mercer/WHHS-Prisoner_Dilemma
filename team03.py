f709abe53dd5db15f7c88c90342da3439145ab70
Price <dprice0354@greenville.k12.sc.us>
2018-03-21 13:08:50 -0400

    This change allows for a more positive win

--- team03.py
+++ team03.py
@@ -1,4 +1,18 @@
-####
+'''
+Dalton Price
+William 
+T3AM
+
+
+
+
+
+
+
+
+
+'''
+
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
@@ -6,11 +20,44 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Shrek' # Only 10 chars displayed.
+strategy_name = 'fun'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
+    if len(their_history) and len (my_history) ==0:
+        return 'b'
+    if len(their_history) and len (my_history) ==1:
+        return 'b'
+    if len(their_history) and len (my_history) ==2:
+        return 'c'
+    if len(their_history) and len (my_history) ==3:
+        return 'b'
+    else:
+        return 'c'
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
