900654473be325e82b758bd7760887b104016473
RokShox <ksease3637@greenvilleschools.us>
2018-03-21 12:36:13 -0400

    modified code

--- team06.py
+++ team06.py
@@ -15,7 +15,7 @@ def move(my_history, their_history, my_score, their_score):
     if (their_history) == '':
         print ('b')
     if their_history == 'c':
-        if my_score >= -10:
+        if my_score >= -1:
             print ('b')
         else:
             print ('c')
